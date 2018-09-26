5e51c2247e206034368c3445cf405fce00167a9d
alvine28 <alvine28@live.com.ph>
2018-09-26 21:17:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express'),
     engines = require('consolidate'),
     assert = require('assert'),
     ObjectId = require('mongodb').ObjectID,
-    url = 'mongodb://alvinejmc:Ppe5ITmHxh50nnmrqQ3WHObI4ZdxnLIq3zKoMAy8EBiBsMWsYBNoJuA9kKNGzu4djtocq9r1pVCT1KXZ1SwXMA==@alvinejmc.documents.azure.com:10255/?ssl=true&replicaSet=globaldb';
+    url = 'mongodb://jmc-db:cWEYUzoh8cczXykwa33eAUpIyjC1FUZvlIiBfi1Q7PDI8garPSTRcWvAaMpvguZAbiwpZpAoJQ5MxKJXsQUumw==@jmc-db.documents.azure.com:10255/?ssl=true&replicaSet=globaldb';
 
 app.use(express.static(__dirname + "/public"));
 

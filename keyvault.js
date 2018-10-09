203bbf3e5d8de2e95875804fc52f5bd88f97d563
Philip Domingo <v-phdomi@microsoft.com>
2018-10-09 15:58:04 +0700

    Added Azure Key Vault

---
+++ keyvault.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+var KeyVault = require('azure-keyvault');
+var AuthenticationContext = require('adal-node').AuthenticationContext;
+var clientId = process.env.CLIENT_ID;
+var clientSecret = process.env.CLIENT_SECRET;
+var vaultBaseUri = process.env.VAULT_URI;
+var Promise = require('bluebird');
+
+// Authenticator - retrieves the access token
+var authenticator = function (challenge, callback) {
+    // Create a new authentication context.
+    var context = new AuthenticationContext(challenge.authorization);
+    // Use the context to acquire an authentication token.
+    return context.acquireTokenWithClientCredentials(challenge.resource, clientId, clientSecret, function (err, tokenResponse) {
+        if (err) throw err;
+        // Calculate the value to be set in the request's Authorization header and resume the call.
+        var authorizationValue = tokenResponse.tokenType + ' ' + tokenResponse.accessToken;
+        return callback(null, authorizationValue);
+    });
+};
+
+var credentials = new KeyVault.KeyVaultCredentials(authenticator);
+var client = new KeyVault.KeyVaultClient(credentials);
+
+var getSecretValue = function(secretName) {
+    return new Promise(function(resolve, reject) {
+        client.getSecret(vaultBaseUri, secretName, '', function(err, result) {
+            if(err) return reject(err);
+            resolve(result.value);
+        });
+    });
+}
+
+var cosmosDbConnectionString = function() {
+    return getSecretValue(process.env.COSMOSDB_SECRET_NAME);
+}
+
+module.exports = {
+    cosmosDbConnectionString: cosmosDbConnectionString
+};
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
@@ -5,7 +6,7 @@ var express = require('express'),
     engines = require('consolidate'),
     assert = require('assert'),
     ObjectId = require('mongodb').ObjectID,
-    url = 'mongodb://alvinejmc:Ppe5ITmHxh50nnmrqQ3WHObI4ZdxnLIq3zKoMAy8EBiBsMWsYBNoJuA9kKNGzu4djtocq9r1pVCT1KXZ1SwXMA==@alvinejmc.documents.azure.com:10255/?ssl=true&replicaSet=globaldb';
+    keyVault = require('./keyvault');
 
 app.use(express.static(__dirname + "/public"));
 
@@ -22,62 +23,65 @@ function errorHandler(err, req, res, next) {
     res.status(500).render("error_template", { error: err});
 }
 
-MongoClient.connect(process.env.MONGODB_URI || url,function(err, db){
-    assert.equal(null, err);
-    console.log('Successfully connected to MongoDB.');
+keyVault.cosmosDbConnectionString().then(function (result) {
+    MongoClient.connect(process.env.MONGODB_URI || result, function (err, db) {
+        assert.equal(null, err);
+        console.log('Successfully connected to MongoDB.');
 
-    var records_collection = db.collection('records');
+        var records_collection = db.collection('records');
 
-    app.get('/records', function(req, res, next) {
-        // console.log("Received get /records request");
-        records_collection.find({}).toArray(function(err, records){
-            if(err) throw err;
+        app.get('/records', function (req, res, next) {
+            // console.log("Received get /records request");
+            records_collection.find({}).toArray(function (err, records) {
+                if (err) throw err;
 
-            if(records.length < 1) {
-                console.log("No records found.");
-            }
-
-            // console.log(records);
-            res.json(records);
-        });
-    });
-
-    app.post('/records', function(req, res, next){
-        console.log(req.body);
-        records_collection.insert(req.body, function(err, doc) {
-            if(err) throw err;
-            console.log(doc);
-            res.json(doc);
-        });
-    });
-
-    app.delete('/records/:id', function(req, res, next){
-        var id = req.params.id;
-        console.log("delete " + id);
-        records_collection.deleteOne({'_id': new ObjectId(id)}, function(err, results){
-            console.log(results);
-            res.json(results);
-        });
-    });
-
-    app.put('/records/:id', function(req, res, next){
-        var id = req.params.id;
-        records_collection.updateOne(
-            {'_id': new ObjectId(id)},
-            { $set: {
-                'name' : req.body.name,
-                'email': req.body.email,
-                'phone': req.body.phone
+                if (records.length < 1) {
+                    console.log("No records found.");
                 }
-            }, function(err, results){
+
+                // console.log(records);
+                res.json(records);
+            });
+        });
+
+        app.post('/records', function (req, res, next) {
+            console.log(req.body);
+            records_collection.insert(req.body, function (err, doc) {
+                if (err) throw err;
+                console.log(doc);
+                res.json(doc);
+            });
+        });
+
+        app.delete('/records/:id', function (req, res, next) {
+            var id = req.params.id;
+            console.log("delete " + id);
+            records_collection.deleteOne({ '_id': new ObjectId(id) }, function (err, results) {
                 console.log(results);
                 res.json(results);
+            });
         });
-    });
 
-    app.use(errorHandler);
-    var server = app.listen(process.env.PORT || 1338, function() {
-        var port = server.address().port;
-        console.log('Express server listening on port %s.', port);
+        app.put('/records/:id', function (req, res, next) {
+            var id = req.params.id;
+            records_collection.updateOne(
+                { '_id': new ObjectId(id) },
+                {
+                    $set: {
+                        'name': req.body.name,
+                        'email': req.body.email,
+                        'phone': req.body.phone
+                    }
+                }, function (err, results) {
+                    console.log(results);
+                    res.json(results);
+                });
+        });
+
+        app.use(errorHandler);
+        var server = app.listen(process.env.PORT || 1338, function () {
+            var port = server.address().port;
+            console.log('Express server listening on port %s.', port);
+        })
     })
-})
+});
